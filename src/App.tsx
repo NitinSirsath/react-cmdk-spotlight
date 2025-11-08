15d5a994db190cb8a92460fb483dbe81092d778d
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-11-08 23:48:26 +0530

    updates and library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import AmrutaHappyBirthday from "./components/birthday";
+import SpotlightBase from "./components/spotlight/spotlight-base";
 
 const App = () => {
   return (
     <div>
       <AmrutaHappyBirthday />
+      <SpotlightBase />
     </div>
   );
 };
